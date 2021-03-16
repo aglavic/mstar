1505f47f0f0f4b35002933dd906b4169a0dd9426
Artur Glavic <artur.glavic@psi.ch>
2021-03-16 19:56:25 +0100

    Add manual calculation of Brilliance from Monitor_nD

--- model/compile_if_needed.sh
+++ model/compile_if_needed.sh
@@ -4,6 +4,6 @@ if [ mstar.instr -nt mstar.out ] || [ ! -f mstar.out ]; then
 	rm mstar.c mstar.out
 	mcstas -o mstar.c mstar.instr $*
 	mpicc -O2 -o mstar.out mstar.c -lm -DUSE_MPI -lmcpl \
-	    -DUSE_NEXUS -lNeXus \
 	   -Wno-unused-result -Wno-format-truncation -Wno-format-overflow -Wno-stringop-overflow
+	   # -DUSE_NEXUS -lNeXus \
 fi
